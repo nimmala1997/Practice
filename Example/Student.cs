34e3d0156b1f66013df303f2d904e72b3222caee
npavankalyan <nimmalapavankalyan1997@gmail.com>
2023-12-22 14:16:35 +0530

    Add all files

---
+++ Example/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Practice.Example
+{
+    public class Student
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine($"My name is {Name}. I am {Age} years old");
+        }
+
+        public void SayHello(string friend)
+        {
+            Console.WriteLine($"{Name} says hello to {friend}");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Practice.Example;
+
+namespace Practice.Program
+{
+    public class Program
+    {
+        public static void Main(string[] Args)
+        {
+            Student student = new Student();
+            student.Age = 26;
+            student.Name = "Pavan";
+            student.Display();
+            student.SayHello("Kalyan");
+            Console.ReadLine();
+        }
+    }
+}
